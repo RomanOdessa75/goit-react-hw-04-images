bcdc078dbb6c2fa950407ac3e0fe892d755d46da
RomanOdessa75 <romeowrc@ukr.net>
2023-05-25 01:50:50 +0300

    react-hw-04-images-1-try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
+import { useEffect, useState } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { fetchImages } from './api/fetchImages';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import React from 'react';
+
+export function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentSearch, setCurrentSearch] = useState('');
+  const [pageNr, setPageNr] = useState(1);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalImg, setModalImg] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setIsLoading(true);
+    const inputForSearch = e.target.elements.inputForSearch;
+    if (inputForSearch.value.trim() === '') {
+      return;
+    }
+    const response = await fetchImages(inputForSearch.value, 1);
+
+    setImages(response);
+    setIsLoading(false);
+    setCurrentSearch(inputForSearch.value);
+    setPageNr(1);
+  };
+
+  const handleClickMore = async () => {
+    const response = await fetchImages(currentSearch, pageNr + 1);
+    setImages([...images, ...response]);
+    setPageNr(pageNr + 1);
+  };
+
+  const handleImageClick = e => {
+    setModalOpen(true);
+    setModalAlt(e.target.alt);
+    setModalImg(e.target.name);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+    setModalImg('');
+    setModalAlt('');
+  };
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        handleModalClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridGap: '16px',
+        paddingBottom: '24px',
       }}
     >
-      React homework template
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <React.Fragment>
+          <Searchbar onSubmit={handleSubmit} />
+          <ImageGallery onImageClick={handleImageClick} images={images} />
+          {images.length > 0 ? <Button onClick={handleClickMore} /> : null}
+        </React.Fragment>
+      )}
+      {modalOpen ? (
+        <Modal src={modalImg} alt={modalAlt} handleClose={handleModalClose} />
+      ) : null}
     </div>
   );
-};
+}
